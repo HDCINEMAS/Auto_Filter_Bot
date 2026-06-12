8011e09bf8e1a9814733ac1069ea34e8432f106b
HDCINEMAS <153760586+HDCINEMAS@users.noreply.github.com>
2026-06-12 14:06:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sivaramanarayanan:siva@2008#%@cluster0.bcjl4gc.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sivaramanarayanan:siva472008@cluster0.bcjl4gc.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
